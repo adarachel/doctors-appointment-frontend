fc7267a0cb764195c2f8ff36d21ad14a7c5715fd
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-10-03 20:19:04 +0400

    Create AddDoctor component

---
+++ src/components/AddDoctor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DoctorForm from './addDoctorForm/DoctorForm'
+import './addDoctorForm/doctor.css';
+
+const AddDcotor = () => (
+  <div className="add-doctor-container">
+    <h1>Add a new Doctor</h1>
+    <DoctorForm />
+  </div>
+);
+
+export default AddDcotor;
