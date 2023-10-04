61b640083d20e3ea27618a8f8827c5e2a0af7fb8
DebyGrey <demifashoro@gmail.com>
2023-10-04 11:05:00 +0100

    Fix linter error

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { createReserve } from '../redux/reservations/ReservationSlice';
+import { createReserve } from '../redux/reservations/reservationSlice';
 import { getDoctors } from '../redux/doctors/doctorsSlice';
 import './componentsCss/reserve.css';
 
