163b0cb68ae530a0872a8c46bb89c56cef3d107d
Ada Rachel <adarachelofficial@gmail.com>
2023-10-04 11:14:59 -0700

    import user slice

--- src/components/signupPage/Signup.js
+++ src/components/signupPage/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import useNavigate from 'react-router';
+import { useNavigate } from 'react-router';
+import { signupUser } from '../../redux/users/userSlice';
 
 const Signup = () => {
     const dispatch = useDispatch();
