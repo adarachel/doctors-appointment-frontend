4105875dd77aeb137bb9d79ab63a64b1cac6b0fa
Ada Rachel <adarachelofficial@gmail.com>
2023-10-04 13:03:14 -0700

    Implement Private Route component to handle user authentication

---
+++ src/components/Private.js
@@ -0,0 +1,16 @@
+import { Navigate } from 'react-router';
+import PropTypes from 'prop-types';
+
+const Private = ({ children }) => {
+    const savedUsername = localStorage.getItem('username');
+
+    if (savedUsername) {
+        return children;
+    }
+    return <Navigate to="/login" />;
+};
+
+Private.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+export default Private;
