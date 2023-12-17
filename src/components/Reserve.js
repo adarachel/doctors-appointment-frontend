4f0eddd6b5d7b0436f399ea60388751586e88212
Ankit <ankit26kushwa@gmail.com>
2023-12-18 01:28:16 +0530

    Fix reserve style

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -57,7 +57,7 @@ const Reservation = () => {
     <div className="container-2">
       <h1 className="heading">Book an Appointment for a Doctor</h1>
       <form onSubmit={submit}>
-        <h2>Make an appointment with us</h2>
+        <h2 className="form-h2">🏡 Make an appointment 🎀 </h2>
         <input
           type="username"
           name="username"
@@ -85,7 +85,7 @@ const Reservation = () => {
           name="time"
           value={reserve.time}
           onChange={handleInputChange}
-          placeholder="Time Duration"
+          placeholder="Time Duration in Minutes"
         />
 
         <div className="select-container">
